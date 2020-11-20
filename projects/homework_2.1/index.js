7877093733f8c7a7911d920fe319d9bc421cfdf4
Ekaterina Iagofarova <ekaterina.yagofarova@gmail.com>
2020-11-20 16:22:38 +0300

    4 tasks was solved

---
+++ projects/homework_2.1/index.js
@@ -0,0 +1,88 @@
+/* ДЗ 2 - работа с массивами и объектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   forEach([1, 2, 3], (el) => console.log(el))
+ */
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
+ */
+function map(array, fn) {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    // текущий элемент передаем в функцию, которую нам дали
+    newArray[i] = fn(array[i], i, array);
+  }
+  return newArray;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   reduce([1, 2, 3], (all, current) => all + current) // 6
+ */
+function reduce(array, fn, initial) {
+  const init = typeof initial != 'undefined';
+
+  let early = init ? initial : array[0];
+
+  for (let i = init ? 0 : 1; i < array.length; i++) {
+    early = fn(early, array[i], i, array);
+  }
+  return early;
+}
+
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+  const features = [];
+
+  for (const name in obj) {
+    features.push(name.toUpperCase());
+  }
+
+  return features;
+}
+
+/*
+ Задание 5 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+
+ Пример:
+   const obj = createProxy({});
+   obj.foo = 2;
+   console.log(obj.foo); // 4
+ */
+function createProxy(obj) {}
+
+export { forEach, map, reduce, upperProps, createProxy };
