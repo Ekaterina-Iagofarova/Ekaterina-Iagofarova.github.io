f57aa5cb0ffa2076c26c711e2bcd52694ee5c9d0
Ekaterina Iagofarova <ekaterina.yagofarova@gmail.com>
2020-12-06 17:11:42 +0300

    homework 6 done

---
+++ projects/dnd/index.js
@@ -0,0 +1,66 @@
+/* Задание со звездочкой */
+
+/*
+ Создайте страницу с кнопкой.
+ При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
+ Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+import './dnd.html';
+
+const homeworkContainer = document.querySelector('#app');
+
+function random(from, to) {
+  return parseInt(from + Math.random() * to - from);
+}
+
+let drag;
+let x = 0;
+let y = 0;
+
+document.addEventListener('mousemove', (e) => {
+  if (drag) {
+    drag.style.left = e.changeX - x + 'px';
+    drag.style.top = e.changeY - y + 'px';
+  }
+});
+
+export function createDiv() {
+  const div = document.createElement('div');
+  const min = 40;
+  const max = 300;
+  const color = 0xffffff;
+
+  div.className = 'draggable-div';
+  div.style.background = '#' + random(0, color).toString(16);
+  div.style.top = random(0, window.innerHeight) + 'px';
+  div.style.left = random(0, window.innerWidth) + 'px';
+  div.style.width = random(min, max) + 'px';
+  div.style.height = random(min, max) + 'px';
+
+  div.addEventListener('mousedown', (e) => {
+    drag = div;
+    x = e.offsetX;
+    y = e.offsetY;
+  });
+
+  div.addEventListener('mouseup', () => (drag = false));
+
+  return div;
+}
+
+const addDivButton = homeworkContainer.querySelector('#addDiv');
+
+addDivButton.addEventListener('click', function () {
+  const div = createDiv();
+  homeworkContainer.appendChild(div);
+});
